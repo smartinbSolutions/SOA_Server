0dab3f231fcc5deb1af4df46c23ca82127e1b71b
Mohammed Shahrour <142575363+DevZeroSh@users.noreply.github.com>
2025-07-01 21:50:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,43 +10,40 @@ const blogCategoryRoutes = require("./routes/blogCategoryRoutes.js");
 const blogRoutes = require("./routes/blogRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const contactInfoRoutes = require("./routes/contactInfoRoutes.js");
+dotenv.config({ path: "config.env" });
 
-dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Middlewares
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-console.log("🧩 Static files will be served from:", path.join(__dirname, "public"));
-app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "uploads")));
 
-// Routes
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
 
+app.use("/api/auth", userRoutes);
+app.use("/api/blog-categories", blogCategoryRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/contactInfo", contactInfoRoutes);
 
-// Test route
-app.get("/", (_req, res) => {
-  res.send("✅ API is up and running with HTTPS!");
+const PORT = process.env.PORT || 8000;
+
+const privateKeyPath = path.join(__dirname, "../PossBackend/pv.key");
+const certificatePath = path.join(__dirname, "../PossBackend/certificata.crt");
+const caficatePath = path.join(__dirname, "../PossBackend/ca.crt");
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const certificate = fs.readFileSync(certificatePath, "utf8");
+const caficate = fs.readFileSync(caficatePath, "utf8");
+
+const credentials = { key: privateKey, cert: certificate, ca: caficate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
+  console.log(`App running on port ${PORT} using HTTPS`);
 });
-
-// Start HTTPS Server
-const startServer = async () => {
-  await connectDB();
-
-  const privateKeyPath = path.join(__dirname, "../PossBackend/pv.key");
-  const certificatePath = path.join(__dirname, "../PossBackend/certificata.crt");
-  const caficatePath = path.join(__dirname, "../PossBackend/ca.crt");
-
-  const privateKey = fs.readFileSync(privateKeyPath, "utf8");
-  const certificate = fs.readFileSync(certificatePath, "utf8");
-  const caficate = fs.readFileSync(caficatePath, "utf8");
-
-  const credentials = { key: privateKey, cert: certificate, ca: caficate };
-
-  https.createServer(credentials, app).listen(PORT, () => {
-    console.log(`🔒 HTTPS Server running at https://localhost:${PORT}`);
-  });
-};
-
-startServer();
