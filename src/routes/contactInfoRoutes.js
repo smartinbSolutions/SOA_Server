096a160a93064e7aee03b9350d9340211060a3af
Mohammed Shahrour <142575363+DevZeroSh@users.noreply.github.com>
2025-07-01 22:39:01 +0300

    Update contactInfoRoutes.js

--- src/routes/contactInfoRoutes.js
+++ src/routes/contactInfoRoutes.js
@@ -1,10 +1,15 @@
-"use strict";
-// src/routes/contactInfo.routes.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const contactInfoController_1 = require("../controllers/contactInfoController");
-const router = (0, express_1.Router)();
-router.get("/", contactInfoController_1.getContactInfo);
-router.put("/", contactInfoController_1.upsertContactInfo);
-router.delete("/", contactInfoController_1.deleteContactInfo);
-exports.default = router;
+// routes/contactInfoRoutes.js
+const express = require("express");
+const {
+  getContactInfo,
+  upsertContactInfo,
+  deleteContactInfo,
+} = require("../controllers/contactInfoController");
+
+const router = express.Router();
+
+router.get("/", getContactInfo);
+router.put("/", upsertContactInfo);
+router.delete("/", deleteContactInfo);
+
+module.exports = router;
