29a224c31d3eb26593ceb208855d3a74786d90b5
Mohammed Shahrour <142575363+DevZeroSh@users.noreply.github.com>
2025-07-01 22:39:25 +0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const usersController_1 = require("../controllers/usersController");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-// 🔓 Public routes
-router.post("/register", auth_middleware_1.requireAuth, usersController_1.register);
-router.get("/users", auth_middleware_1.requireAuth, usersController_1.getAllUsers);
-router.put("/users/:id", auth_middleware_1.requireAuth, usersController_1.updateUserController);
-router.post("/login", usersController_1.login);
-exports.default = router;
+// routes/userRoutes.js
+const express = require("express");
+const {
+  register,
+  getAllUsers,
+  updateUserController,
+  login,
+} = require("../controllers/usersController");
+const { requireAuth } = require("../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.post("/register", requireAuth, register);
+router.get("/users", requireAuth, getAllUsers);
+router.put("/users/:id", requireAuth, updateUserController);
+router.post("/login", login);
+
+module.exports = router;
